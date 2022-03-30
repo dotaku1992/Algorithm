daced3926a0a4ba7510c32f5500a90eca48f6dec
dotaku1992 <dotaku1992@gmail.com>
2022-03-30 23:18:10 +0900

    file name change [mistake]

--- Baekjoon/17219 비밀번호 찾기.py
+++ Baekjoon/17219 비밀번호 찾기.py
@@ -1,11 +1,11 @@
 import sys
 from collections import defaultdict
 
-n,m = map(int,sys.stdin.readline().rstrip().split())
+n, m = map(int, sys.stdin.readline().rstrip().split())
 adressAndPassword = defaultdict(str)
 for _ in range(n):
-    ads,password = sys.stdin.readline().rstrip().split()
-    adressAndPassword[ads]=password
+    ads, password = sys.stdin.readline().rstrip().split()
+    adressAndPassword[ads] = password
 
 for _ in range(m):
     ads = sys.stdin.readline().rstrip()
