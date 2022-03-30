6d12600d3f0c438f432fc03e85f03e7a1dc7099e
dotaku1992 <dotaku1992@gmail.com>
2022-03-30 23:16:04 +0900

    py

---
+++ Baekjoon/13706 제곱근.py
@@ -0,0 +1,12 @@
+import sys
+from collections import defaultdict
+
+n,m = map(int,sys.stdin.readline().rstrip().split())
+adressAndPassword = defaultdict(str)
+for _ in range(n):
+    ads,password = sys.stdin.readline().rstrip().split()
+    adressAndPassword[ads]=password
+
+for _ in range(m):
+    ads = sys.stdin.readline().rstrip()
+    print(adressAndPassword[ads])
