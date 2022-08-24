dd942cce6372af6712b076c56f20b1058fcffda8
dotaku1992 <dotaku1992@gmail.com>
2022-08-25 07:22:01 +0900

    8/25

--- Baekjoon/25501 재귀의 귀재.py
+++ Baekjoon/25501 재귀의 귀재.py
@@ -6,12 +6,12 @@ for _ in range(T):
     text = sys.stdin.readline().rstrip()
     isPalindrome = True
     anscnt = 0
-    for i in range(len(text)//2):
-        anscnt+=1
-        if text[i]!=text[-(i+1)]:
+    for i in range(len(text) // 2):
+        anscnt += 1
+        if text[i] != text[-(i + 1)]:
             isPalindrome = False
             break
     else:
-        anscnt+=1
+        anscnt += 1
 
-    print(int(isPalindrome),anscnt)
+    print(int(isPalindrome), anscnt)
