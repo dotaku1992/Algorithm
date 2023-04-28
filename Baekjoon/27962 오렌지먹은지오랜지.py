46a12f62dc62251901ed0277b5993259bbf27b9d
dotaku1992 <dotaku1992@gmail.com>
2023-04-28 22:29:26 +0900

    통과여부 : n -> 틀렸습니다. 푼날짜 : 23/4/28 solved.ac : B1 문제분류 : 구현,문자열 새로운 내용 학습 여부 : Y 1. 부분 문자열의 길이는 1이상이라 하길래 무조건 한 글자만 다르면 비타민 문자열로 만들 수 있을거라 생각했지만 틀린 내용이 있는 것 같다.

--- Baekjoon/27962 오렌지먹은지오랜지.py
+++ Baekjoon/27962 오렌지먹은지오랜지.py
@@ -2,22 +2,9 @@ import sys
 from collections import defaultdict
 
 n = int(sys.stdin.readline().rstrip())
-texts = list(sys.stdin.readline().rstrip())
-check = defaultdict(str)
-flag = True
-for textlen in range(1, n + 1):
-    for idx, c in enumerate(texts):
-        if idx + textlen > n:
-            continue
-        else:
-            for sidx in range(idx, idx + textlen):
-                savec = texts[sidx]
-                texts[sidx] = '?'
-                changedtxt = ''.join(texts[idx:idx + textlen])
-                texts[sidx] = savec
-                if changedtxt in check and check[changedtxt] != savec:
-                    flag = False
-                else:
-                    check[changedtxt] = savec
+texts = sys.stdin.readline().rstrip()
+cnt = defaultdict(int)
+for c in texts:
+    cnt[c] = 1
 
-print("YES" if not flag else "No")
+print("YES" if len(cnt.keys()) >= 2 else "NO")
