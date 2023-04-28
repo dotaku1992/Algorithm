08596d7e27d1e1b880e410937837e625ad31bc87
dotaku1992 <dotaku1992@gmail.com>
2023-04-28 23:33:38 +0900

    통과여부 : y 푼날짜 : 23/4/28 solved.ac : B1 문제분류 : 구현,문자열 새로운 내용 학습 여부 : Y 1. 비교하는 두가지의 부분 문자열의 조건을 챙기지 못했었다. 조건은 (1) 맨왼쪽을 포함하는 (2) 맨오른쪽을 포함하는 것을 놓쳤었다.

--- Baekjoon/27962 오렌지먹은지오랜지.py
+++ Baekjoon/27962 오렌지먹은지오랜지.py
@@ -1,10 +1,26 @@
 import sys
-from collections import defaultdict
+
+
+def isMatch(str1, str2):
+    life = 1
+    for c1, c2 in zip(str1, str2):
+        if c1 == c2:
+            continue
+        else:
+            life -= 1
+
+    if life == 0:
+        return True
+    else:
+        return False
+
 
 n = int(sys.stdin.readline().rstrip())
 texts = sys.stdin.readline().rstrip()
-cnt = defaultdict(int)
-for c in texts:
-    cnt[c] = 1
 
-print("YES" if len(cnt.keys()) >= 2 else "NO")
+flag = False
+for textlen in range(1, n):
+    result = isMatch(texts[:textlen], texts[-textlen:])
+    flag |= result
+
+print("YES" if flag else "NO")
