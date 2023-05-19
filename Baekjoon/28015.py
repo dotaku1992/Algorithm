2521f5820ed2ec0790662f04152f9a347cf3d006
dotaku1992 <dotaku1992@gmail.com>
2023-05-19 23:06:33 +0900

    통과여부 : Y 푼날짜 : 23/5/19 solved.ac : S3 문제분류 : 그리디 알고리즘 새로운 내용 학습 여부 : Y 1. 최대한 값은 (,)로 묶어주자

--- Baekjoon/28015.py
+++ Baekjoon/28015.py
@@ -13,6 +13,13 @@ for _ in range(n):
         else:
             checkarr.append(ele)
             cnt[ele] += 1
-    ans = ans + cnt[1] + cnt[2]
-    
+    cnt1, cnt2 = 0, 0
+    for ele in checkarr:
+        if ele == 0:
+            ans += (min(cnt1, cnt2) + (1 if max(cnt1, cnt2) > 0 else 0))
+            cnt1, cnt2 = 0, 0
+        else:
+            cnt1, cnt2 = cnt1 + (ele == 1), cnt2 + (ele == 2)
+    ans += (min(cnt1, cnt2) + (1 if max(cnt1, cnt2) > 0 else 0))
+
 print(ans)
