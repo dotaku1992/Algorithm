a3a1c2659ca2fe0b8b62554a65b01f9e7c8ac8ba
dotaku1992 <dotaku1992@gmail.com>
2023-08-18 20:37:44 +0900

    통과여부 : Y 푼날짜 : 23/8/18 solved.ac : B3 문제분류 : 구현, 문자열 새로운 내용 학습 여부 : Y 파이썬의 datetime 모듈을 통해서 시간의 대소를 비교할 수 있다. date(y,m,d)를 통해 객체를 생성한뒤 대소를 비교하고 좀 더 미래의 날짜가 큰값이다.

--- Baekjoon/28454.py
+++ Baekjoon/28454.py
@@ -1,24 +1,10 @@
 import sys
+from datetime import date
 
-my, mm, md = map(int, sys.stdin.readline().rstrip().split('-'))
-
+currentdate = date(*map(int, sys.stdin.readline().rstrip().split('-')))
 cnt = 0
 for _ in range(int(sys.stdin.readline().rstrip())):
-    iy, im, id = map(int, sys.stdin.readline().rstrip().split('-'))
-    arr = []
-    for m, i in zip([my, mm, md], [iy, im, id]):
-        result = 1 if i > m else 0 if i == m else -1
-        arr.append(result)
-    flag = False
-    for ele in arr:
-        flag = (ele == 1) | flag
-        if ele == 0 or ele == 1:
-            continue
-        else:
-            if flag:
-                continue
-            else:
-                break
-    else:
+    nxtdate = date(*map(int, sys.stdin.readline().rstrip().split('-')))
+    if nxtdate >= currentdate:
         cnt += 1
 print(cnt)
